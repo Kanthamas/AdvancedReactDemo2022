387b364510a11a24d45f168c4f696248c7a7fee8
auntieMarch <auntiemarch@gmail.com>
2022-11-10 15:17:39 +0700

    feature: added css in ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react"; // JSX
 import ReactDOM from "react-dom/client";
 import "./index.css";
-=======
-import React, { useEffect, useState } from 'react'; // JSX
-import ReactDOM from 'react-dom/client';
-import './index.css';
 // import App from './Components/App';
 // import MyComponent from './Components/MyComponent';
-import MyComponent2 from './Components/MyComponent2';
-import reportWebVitals from './reportWebVitals';
-import MyContextProvider from './Components/MyContextProvider';
->>>>>>> f3ba24b23f9347a44b5aa42209fd875ff255b882
+// import MyComponent2 from './Components/MyComponent2';
 
 import reportWebVitals from "./reportWebVitals";
-import MyContextComponent from "./Components/MyContextComponent";
+import MyContextComponent from "./Components/MyContextProvider";
 import ErrorBoundary from "./Components/ErrorBoundary";
 import MyMagicComponent from "./Components/MyMagicComponent";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
 	<React.StrictMode>
 		<MyContextComponent>
 			{/* <App /> */}
@@ -30,19 +22,6 @@ root.render(
 			</ErrorBoundary>
 		</MyContextComponent>
 	</React.StrictMode>
-=======
-  <React.StrictMode>
-  <MyContextProvider>
-  { /* <App /> */ }
-  { /* <MyComponent name="Gawee" /> */ }
-  { /* <MyChatContextProvider>
-      <Chat />
-      <ChatToolbar />
-</MyChatContextProvider>*/ }
-    <MyComponent2 />
-  </MyContextProvider>
-  </React.StrictMode>
->>>>>>> f3ba24b23f9347a44b5aa42209fd875ff255b882
 );
 
 // If you want to start measuring performance in your app, pass a function
